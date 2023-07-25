7a6f3eae110def4f4bc97c40e3c52a0dc6928e18
Adam Alexander <95765425+asdalexander@users.noreply.github.com>
2023-07-25 13:09:42 -0400

    Update 02_MultiQC.sh

--- pipeline_local/02_Trim/02_MultiQC.sh
+++ pipeline_local/02_Trim/02_MultiQC.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+########## SLURM HEADER START ##########
 #SBATCH --time=2:00:00
 #SBATCH --job-name=MultiQC
 #SBATCH --partition=Orion
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=8GB
+########## SLURM HEADER END ##########
 
+########## DESCRIPTION ##########
+# Run MultiQC on each of the trimmed FASTQ reads.
+# Output can be used to compare quality of reads between 
+# trimming of adapter sequences and low-quality reads.
+
+########## SCRIPT START ##########
 echo "======================================================"
 echo "Start Time  : $(date)"
 echo "Submit Dir  : $SLURM_SUBMIT_DIR"
@@ -16,7 +24,7 @@ echo ""
 module load multiqc
 
 #update this if running the pipeline from a different directory
-TMP_PATH=/nobackup/mougeots_research/adam_alexander/pipeline
+TMP_PATH=/pipeline/absolute/directory
 
 IN_DIR=$TMP_PATH/02_Trim/01_output_data
 OUT_DIR=$TMP_PATH/02_Trim/02_output_data/output_data
