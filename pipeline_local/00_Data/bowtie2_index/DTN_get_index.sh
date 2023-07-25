00cfc4c1a752bc7bab61be639d30328de430a478
Adam Alexander <95765425+asdalexander@users.noreply.github.com>
2023-07-25 11:38:42 -0400

    Update DTN_get_index.sh

--- pipeline_local/00_Data/bowtie2_index/DTN_get_index.sh
+++ pipeline_local/00_Data/bowtie2_index/DTN_get_index.sh
@@ -6,6 +6,14 @@
 #SBATCH --mem=4GB
 ########## SLURM HEADER END ##########
 
+echo "======================================================"
+echo "Start Time  : $(date)"
+echo "Submit Dir  : $SLURM_SUBMIT_DIR"
+echo "Job ID/Name : $SLURM_JOBID / $SLURM_JOB_NAME"
+echo "Num Tasks   : $SLURM_NTASKS total [$SLURM_NNODES nodes @ $SLURM_CPUS_ON_NODE CPUs/node]"
+echo "======================================================"
+echo ""
+
 ########## DESCRIPTION ##########
 # Download the index files required by the BWA aligner. This set of files is sourced from the same
 # NCBI FTP site as the GRCh38.fna reference file and will only work with the *_noalt genome version, 
@@ -15,3 +23,9 @@
 ########## SCRIPT START ##########
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz
 
+
+echo ""
+echo "======================================================"
+echo "JOB COMPLETE"
+echo "End Time   : $(date)"
+echo "======================================================"
