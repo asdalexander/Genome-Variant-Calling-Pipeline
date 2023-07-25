710519e0c6002982e0821a545a1490d62f34e34a
Adam Alexander <95765425+asdalexander@users.noreply.github.com>
2023-07-25 13:33:03 -0400

    Update 04b_MultiQC.sh

--- pipeline_local/04_Sort/04b_MultiQC.sh
+++ pipeline_local/04_Sort/04b_MultiQC.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+########## SLURM HEADER START ##########
 #SBATCH --time=20:00:00
 #SBATCH --job-name=MQC-QualiMap
 #SBATCH --partition=Orion
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=96GB
+#SBATCH --ntasks-per-node=18
+#SBATCH --mem=36GB
+########## SLURM HEADER END ##########
 
+########## DESCRIPTION ##########
+# 04_Sort contains a series of five scripts which perform GATK-recommended steps to further process 
+# reads before variant calling. 
+# MultiQC generates summary-level stats across the QC reports of a population of samples. 
+
+########## SCRIPT START ##########
 echo "======================================================"
 echo "Start Time  : $(date)"
 echo "Submit Dir  : $SLURM_SUBMIT_DIR"
@@ -14,7 +22,7 @@ echo "======================================================"
 echo ""
 
 #update this if running the pipeline from a different directory
-TMP_PATH=/nobackup/mougeots_research/adam_alexander/pipeline
+TMP_PATH=/pipeline/absolute/directory
 
 IN_DIR=$TMP_PATH/04_Sort/04a_output_data
 OUT_DIR=$TMP_PATH/04_Sort/04b_output_data
