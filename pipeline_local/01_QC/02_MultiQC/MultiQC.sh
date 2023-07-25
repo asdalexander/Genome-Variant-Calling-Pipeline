120acc40f13bf66f8067d4c13bf4336a86ec2026
Adam Alexander <95765425+asdalexander@users.noreply.github.com>
2023-07-25 12:53:11 -0400

    Update MultiQC.sh

--- pipeline_local/01_QC/02_MultiQC/MultiQC.sh
+++ pipeline_local/01_QC/02_MultiQC/MultiQC.sh
@@ -5,19 +5,22 @@
 #SBATCH --ntasks-per-node=24
 #SBATCH --mem=48GB
 
+########## DESCRIPTION ##########
+# Run MultiQC on the entire directory of FASTQC files generated from the previous step. 
+# If manually triggering this script, you must wait for FastQC to complete for each of the original FASTQ files. 
+
 echo "======================================================"
 echo "Start Time   : $(date)"
 echo "Submit Dir   : $SLURM_SUBMIT_DIR"
 echo "Job ID/Name  : $SLURM_JOBID / $SLURM_JOB_NAME"
 echo "Num Tasks    : $SLURM_NTASKS total [$SLURM_NNODES nodes @ $SLURM_CPUS_ON_NODE CPUs/node]"
-echo "Compute Node : $(/bin/hostname -s)"
 echo "======================================================"
 echo ""
 
 module load multiqc
 
 #update this if running the pipeline from a different directory
-TMP_PATH=/nobackup/mougeots_research/adam_alexander/pipeline
+TMP_PATH=/pipeline/absolute/directory
 
 IN_DIR=$TMP_PATH/01_QC/01_FastQC/output_data
 OUT_DIR=$TMP_PATH/01_QC/02_MultiQC/output_data
