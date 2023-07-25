63c6c66334ef1cd1e79842da3b12867cc2731d5f
Adam Alexander <95765425+asdalexander@users.noreply.github.com>
2023-07-25 11:38:39 -0400

    Update create_index.sh

--- pipeline_local/00_Data/bowtie2_index/create_index.sh
+++ pipeline_local/00_Data/bowtie2_index/create_index.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
-#SBATCH --time=120:00:00
+########## SLURM HEADER START ##########
+#SBATCH --time=24:00:00
 #SBATCH --job-name=generate_bwa_index
 #SBATCH --partition=Orion
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=100GB
+#SBATCH --ntasks-per-node=24
+#SBATCH --mem=48GB
+########## SLURM HEADER END ##########
 
-#Create the index files required by the BWA aligner
+########## DESCRIPTION ##########
+# Create the index files required by the BWA aligner
 
-IN_DIR=/nobackup/mougeots_research/adam_alexander/exome_pipeline/00_Data/reference/GRCh38.fna
-OUT_DIR=/nobackup/mougeots_research/adam_alexander/exome_pipeline/00_Data/bowtie2_index
+########## SCRIPT START ##########
+echo "======================================================"
+echo "Start Time  : $(date)"
+echo "Submit Dir  : $SLURM_SUBMIT_DIR"
+echo "Job ID/Name : $SLURM_JOBID / $SLURM_JOB_NAME"
+echo "Num Tasks   : $SLURM_NTASKS total [$SLURM_NNODES nodes @ $SLURM_CPUS_ON_NODE CPUs/node]"
+echo "======================================================"
+echo ""
 
+
+# Set the TMP_PATH directory
+TMP_PATH=/pipeline/absolute/directory
+
+IN_DIR=$TMP_PATH/00_Data/reference/GRCh38.fna
+OUT_DIR=$TMP_PATH/00_Data/bowtie2_index
+
+# Load bowtie2 (bowtie2 must be installed)
 module load bowtie2
 
+# Change directory into IN_DIR and begin bowtie2-build process on files in the IN_DIR
 cd IN_DIR
 
 bowtie2-build \
 $IN_DIR
 
+# Move bowtie2 index files (those ending in .bt2) to OUT_DIR 
 mv *.bt2 $OUT_DIR
+
+
+echo ""
+echo "======================================================"
+echo "JOB COMPLETE"
+echo "End Time   : $(date)"
+echo "======================================================"
